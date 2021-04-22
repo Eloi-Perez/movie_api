407488fdb1d5777750f049dac8f9402565c1788f
Eloi-Perez <eloi104@hotmail.com>
2021-04-22 12:20:53 +0100

    improved auth

--- auth.js
+++ auth.js
@@ -3,10 +3,10 @@ const jwt = require('jsonwebtoken'),
 
 require('./passport.js');
 
-let generateJWTToken = (user) => {
-    return jwt.sign(user, process.env.JWT_KEY, {
-        subject: user.Username, // Username to encode in the JWToken
-        expiresIn: '7d',
+let generateJWTToken = (payload) => {
+    return jwt.sign(payload, process.env.JWT_KEY, {
+        subject: payload.Username, // Username to encode in the JWToken
+        expiresIn: '14d',
         algorithm: 'HS256'
     });
 }
@@ -26,7 +26,11 @@ module.exports = (router) => {
                 if (error) {
                     res.send(error);
                 }
-                let token = generateJWTToken(user.toJSON());
+                let payload = {}
+                payload._id = user._id;
+                payload.Username = user.Username;
+                // let token = generateJWTToken(user.toJSON()); // this generate token with full user info
+                let token = generateJWTToken(payload);
                 let Username = user.Username;
                 return res.json({ Username, token });
             });
